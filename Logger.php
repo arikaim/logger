a69157f469e98f365c229fc715ed460034a7d430
Arikaim <sales@intersoft.biz>
2020-10-27 16:34:25 +0200

    =

--- Logger.php
+++ Logger.php
@@ -10,19 +10,31 @@
 namespace Arikaim\Core\Logger;
 
 use Monolog\Logger as MonologLogger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\AbstractHandler;
-use Monolog\Formatter\FormatterInterface;
+use Monolog\Handler\HandlerInterface;
+use Monolog\Formatter\NormalizerFormatter;
 
 use Arikaim\Core\Utils\File;
 use Arikaim\Core\Logger\JsonLogsFormatter;
 use Arikaim\Core\Logger\LogsProcessor;
+use Exception;
 
 /**
  * Logger
  */
 class Logger
 {
+    const DEFAULT_HANDLER = 'file';
+
+    /**
+     * Handler names
+     *
+     * @var array
+     */
+    private $handerNames = [
+        'db',
+        'file'
+    ];
+
     /**
      * Logger object
      *
@@ -38,13 +50,6 @@ class Logger
     protected $enabled;
 
     /**
-     * Logs file name
-     *
-     * @var string
-     */
-    private $fileName;
-
-    /**
      * Logs directory
      *
      * @var string
@@ -52,39 +57,134 @@ class Logger
     private $logsDir;
 
     /**
-     * Logs Formatter
+     * Current handler name
      *
-     * @var FormatterInterface
+     * @var string
      */
-    private $formatter;
-
-    /**
-     * Logs handler
-     *
-     * @var AbstractHandler
-     */
-    private $handler;
+    private $handlerName;
 
     /**
      * Constructor
      *
      * @param string $logsDir
-     * @param AbstractHandler $handler
-     * @param FormatterInterface $formatter
-     * @param string $fileName
+     * @param bool $enabled
+     * @param string|null $handlerName
      */
-    public function __construct($logsDir, AbstractHandler $handler = null, FormatterInterface $formatter = null, $fileName = null) 
-    {         
-        $this->fileName = (empty($fileName) == true) ? 'errors.log' : $fileName;
+    public function __construct($logsDir, $enabled, $handlerName = Self::DEFAULT_HANDLER) 
+    {                
         $this->logsDir = $logsDir;      
-        $this->enabled = true;
-        $this->handler = (empty($handler) == true) ? new StreamHandler($this->getLogsFileName(), MonologLogger::DEBUG) : $handler;
-        $this->formatter = (empty($formatter) == true) ? new JsonLogsFormatter() : $formatter;
-        $this->handler->setFormatter($this->formatter); 
+        $this->enabled = $enabled;
 
         $this->logger = new MonologLogger('system');                
-        $this->logger->pushHandler($this->handler);
-        $this->logger->pushProcessor(new LogsProcessor());         
+        $this->logger->pushProcessor(new LogsProcessor());  
+
+        $handlerName = ($this->isValidHandlerName($handlerName) == true) ? $handlerName : Self::DEFAULT_HANDLER;
+        $this->setHandler($handlerName);
+    }
+
+    /**
+     * Return true if handler name is valid
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public function isValidHandlerName($name)
+    {
+        return \in_array($name,$this->handerNames);
+    }
+
+    /**
+     * Get handler name
+     *
+     * @return string
+     */
+    public function getHandelerName()
+    {
+        return $this->handlerName;
+    }
+
+    /**
+     *  Get handler names
+     */
+    public function getHandlerNames()
+    {
+        return $this->handerNames;
+    }
+
+    /**
+     * Create handler instance
+     *
+     * @param string $name
+     * @return HandlerInterface
+     * @throws Exception
+     */
+    public function createHandler($name): HandlerInterface
+    {
+        switch($name) {
+            case 'file':
+                $handler = new \Monolog\Handler\StreamHandler($this->logsDir,MonologLogger::DEBUG); 
+                $formatter = new JsonLogsFormatter();
+                $handler->setFormatter($formatter); 
+                break;
+
+            case 'db':
+                $handler = new \Arikaim\Core\Logger\Handler\DbHandler(); 
+                $formatter = new NormalizerFormatter();
+                $handler->setFormatter($formatter); 
+                break;
+
+            default:
+                throw new Exception('Not valid log handler name', 1);   
+        }
+
+        return $handler;
+    }
+
+    /**
+     * Replace all handlers with one
+     *
+     * @param string $name
+     * @return void
+     */
+    public function setHandler($name)
+    {
+        $handler = $this->createHandler($name);
+        $this->handlerName = $name;
+
+        $this->setHandlers([$handler]);
+    }
+
+    /**
+     * Set handlers
+     *
+     * @param array $handlers
+     * @return void
+     */
+    public function setHandlers(array $handlers)
+    {
+        $this->logger->setHandlers($handlers);
+    }
+
+    /**
+     * Push handler
+     *
+     * @param string $name
+     * @return void
+     */
+    public function pushHandler($name)
+    {
+        $handler = $this->createHandler($name);
+
+        $this->logger->pushHandler($handler);
+    }
+
+    /** Pop handler
+     *
+     * @return HandlerInterface
+     */
+    public function popHandler(): HandlerInterface
+    {
+        return $this->logger->popHandler();
     }
 
     /**
@@ -98,23 +198,13 @@ class Logger
     }
 
     /**
-     * Get formatter
-     *
-     * @return FormatterInterface
-     */
-    public function getFrmatter()
-    {
-        return $this->formatter;
-    }
-
-    /**
      * Get handler
      *
-     * @return AbstractHandler
+     * @return array
      */
-    public function getHandler()
+    public function getHandlers()
     {
-        return $this->handler;
+        return $this->logger->getHandlers();
     }
 
     /**
@@ -124,7 +214,7 @@ class Logger
      */
     public function getLogsFileName()
     {
-        return $this->logsDir . $this->fileName;
+        return $this->logsDir;
     }
 
     /**
@@ -134,7 +224,31 @@ class Logger
      */
     public function deleteSystemLogs()
     {
-        return (File::exists($this->getLogsFileName()) == false) ? true : File::delete($this->getLogsFileName());
+        $handlers = $this->getHandlers();
+
+        foreach ($handlers as $handler) {
+            $this->deleteLogs($handler);
+        }  
+    }
+
+    /**
+     * Delete logs
+     *
+     * @param object $handler
+     * @return bool
+     */
+    protected function deleteLogs($handler)
+    {     
+        switch (\get_class($handler)) {
+            case 'Monolog\Handler\StreamHandler':
+                return (File::exists($this->getLogsFileName()) == false) ? true : File::delete($this->getLogsFileName());
+            
+            case 'Arikaim\Core\Logger\Handler\DbHandler': {
+                return $handler->getLogsStorage()->getQuery()->delete();
+            }            
+        }
+        
+        return false;
     }
 
     /**
@@ -162,8 +276,8 @@ class Logger
      */
     public function __call($name, $arguments)
     {           
-        $message = $arguments[0];
-        $context = isset($arguments[1]) ? $arguments[1] : [];
+        $message = $arguments[0] ?? '';
+        $context = $arguments[1] ?? [];
 
         return ($this->enabled == true) ? $this->logger->{$name}($message,$context) : false;          
     }
@@ -188,9 +302,21 @@ class Logger
      * @param array $context
      * @return boolean
      */
-    public function error($message, $context = [])
+    public function error($message, array $context = [])
     {      
         return ($this->enabled == true) ? $this->logger->error($message,$context) : false;      
+    }
+
+    /**
+     * Add info log
+     *
+     * @param string $message
+     * @param array $context
+     * @return boolean
+    */
+    public function info($message, array $context = [])
+    {
+        return ($this->enabled == true) ? $this->logger->info($message,$context) : false; 
     }
 
     /**
